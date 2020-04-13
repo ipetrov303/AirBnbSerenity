60b1b82e717400fee878609eb904ca037ef6452d
Doncho Gluharov <dgluharov@gmail.com>
2020-04-13 16:19:16 +0300

    implement Regex for calculation of price

--- src/test/java/com/qualityhouse/serenity/steps/definitions/ReservationDetailsStepsDefinitions.java
+++ src/test/java/com/qualityhouse/serenity/steps/definitions/ReservationDetailsStepsDefinitions.java
@@ -1,10 +1,16 @@
 package com.qualityhouse.serenity.steps.definitions;
 
+import com.qualityhouse.serenity.entities.VacationDetails;
 import com.qualityhouse.serenity.page_objects.ReservationDetailsPage;
 import com.qualityhouse.serenity.steps.libraries.ReservationDetailsActions;
 import cucumber.api.java.en.Then;
 import net.thucydides.core.annotations.Steps;
 import org.assertj.core.api.SoftAssertions;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static com.qualityhouse.serenity.page_objects.ReservationDetailsPage.*;
 
@@ -12,24 +18,25 @@ public class ReservationDetailsStepsDefinitions {
 
     @Steps
     private ReservationDetailsActions bob;
-    private ReservationDetailsPage details;
+    @Steps
+    private ReservationDetailsPage reservationDetails;
+    @Steps
+    private VacationDetails vacationDetails;
+
 
     @Then("^correct total price and reservation details are displayed$")
     public void correctDetailsAreDisplayed() {
 
-        int dailyPrice = 0;
         int taxForService = 0;
         int finalPrice = 0;
         int actualNumberOfPeople = 0;
 
         try {
-            dailyPrice = bob.readsPrice(DAILY_PRICE_FOR_ONE_DAY);
             taxForService = bob.readsPrice(TAX_FOR_SERVICE);
             finalPrice = bob.readsPrice(TOTAL_PRICE);
             actualNumberOfPeople = bob.readsNumberOfPeopleAsInteger(NUMBER_OF_PEOPLE);
 
-            if (dailyPrice == 0) {
-                dailyPrice = bob.readsPrice(DAILY_PRICE_FOR_ONE_DAY1);
+            if (taxForService == 0) {
                 taxForService = bob.readsPrice(TAX_FOR_SERVICE1);
                 finalPrice = bob.readsPrice(TOTAL_PRICE1);
                 actualNumberOfPeople = bob.readsNumberOfPeopleAsInteger(NUMBER_OF_PEOPLE1);
@@ -40,17 +47,36 @@ public class ReservationDetailsStepsDefinitions {
         }
 
 
+        String pattern = "(\\d+)";
+        Pattern regex = Pattern.compile(pattern);
+        Matcher matcher = regex.matcher(bob.readsTextFrom(reservationDetails.priceMultiplyNightsText));
+        List<Integer> valuesForUnitPriceAndNumberOfDays= new ArrayList<>();
+
+        while (matcher.find()) {
+            valuesForUnitPriceAndNumberOfDays.add(Integer.parseInt(matcher.group() + ""));
+        }
+
+        int pricePerNight = valuesForUnitPriceAndNumberOfDays.get(0);
+        int numberOfNights = valuesForUnitPriceAndNumberOfDays.get(1);
+        int weeklyDiscount = Integer.parseInt(reservationDetails.weeklyDiscountField.getText().substring(2));
+
+
         SoftAssertions softly = new SoftAssertions();
 
-        int expectedTotalPrice = (dailyPrice * HomeStepsDefinitions.vacationDetails.getPeriodLength()) + taxForService;
+        int expectedTotalPrice = ((pricePerNight * numberOfNights) + taxForService) - weeklyDiscount;
         int expectedNumberOfGuests = HomeStepsDefinitions.vacationDetails.getAdults()
                 + HomeStepsDefinitions.vacationDetails.getChildren();
+
+        softly.assertThat(numberOfNights)
+                .as("Number of nights should be displayed correctly: ")
+                .isEqualTo(HomeStepsDefinitions.vacationDetails.getPeriodLength());
 
         softly.assertThat(actualNumberOfPeople)
                 .as("Number of guests displayed correctly: ")
                 .isEqualTo(expectedNumberOfGuests);
 
-        softly.assertThat(finalPrice).as("Final price should be calculated correctly: ")
+        softly.assertThat(finalPrice)
+                .as("Final price should be calculated correctly: ")
                 .isEqualTo(expectedTotalPrice);
 
         softly.assertAll();
