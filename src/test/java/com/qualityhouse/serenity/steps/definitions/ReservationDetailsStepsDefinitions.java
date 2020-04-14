ce9e9933896121df2a8bb0f8f0c9b3a72d6f5765
Doncho Gluharov <dgluharov@gmail.com>
2020-04-14 17:55:12 +0300

    refactoring of locators in ReservationDetails

--- src/test/java/com/qualityhouse/serenity/steps/definitions/ReservationDetailsStepsDefinitions.java
+++ src/test/java/com/qualityhouse/serenity/steps/definitions/ReservationDetailsStepsDefinitions.java
@@ -26,31 +26,33 @@ public class ReservationDetailsStepsDefinitions {
 
     @Then("^correct total price and reservation details are displayed$")
     public void correctDetailsAreDisplayed() {
+        String pattern = "(\\d+)";
+        Pattern regex = Pattern.compile(pattern);
+        Matcher matcher = regex.matcher("test");
+        List<Integer> valuesForUnitPriceAndNumberOfDays = new ArrayList<>();
 
         int taxForService = 0;
         int finalPrice = 0;
         int actualNumberOfPeople = 0;
+        int weeklyDiscount = 0;
 
         try {
             taxForService = bob.readsPrice(TAX_FOR_SERVICE);
             finalPrice = bob.readsPrice(TOTAL_PRICE);
             actualNumberOfPeople = bob.readsNumberOfPeopleAsInteger(NUMBER_OF_PEOPLE);
+            matcher = regex.matcher(bob.readsTextFrom(PRICE_MULTIPLY_NIGHTS_TEXT_LOCATOR));
+            weeklyDiscount = Integer.parseInt(bob.readsTextFrom(WEEKLY_DISCOUNT_FIELD_LOCATOR).substring(2));
 
             if (taxForService == 0) {
                 taxForService = bob.readsPrice(TAX_FOR_SERVICE1);
                 finalPrice = bob.readsPrice(TOTAL_PRICE1);
                 actualNumberOfPeople = bob.readsNumberOfPeopleAsInteger(NUMBER_OF_PEOPLE1);
+                matcher = regex.matcher(bob.readsTextFrom(PRICE_MULTIPLY_NIGHTS_TEXT_LOCATOR1));
+                weeklyDiscount = Integer.parseInt(bob.readsTextFrom(WEEKLY_DISCOUNT_FIELD_LOCATOR1).substring(2));
             }
-
         } catch (RuntimeException exception) {
             System.out.println(exception.getMessage());
         }
-
-
-        String pattern = "(\\d+)";
-        Pattern regex = Pattern.compile(pattern);
-        Matcher matcher = regex.matcher(bob.readsTextFrom(reservationDetails.priceMultiplyNightsText));
-        List<Integer> valuesForUnitPriceAndNumberOfDays= new ArrayList<>();
 
         while (matcher.find()) {
             valuesForUnitPriceAndNumberOfDays.add(Integer.parseInt(matcher.group() + ""));
@@ -58,7 +60,6 @@ public class ReservationDetailsStepsDefinitions {
 
         int pricePerNight = valuesForUnitPriceAndNumberOfDays.get(0);
         int numberOfNights = valuesForUnitPriceAndNumberOfDays.get(1);
-        int weeklyDiscount = Integer.parseInt(reservationDetails.weeklyDiscountField.getText().substring(2));
 
 
         SoftAssertions softly = new SoftAssertions();
